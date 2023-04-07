cac82cfb315a73f17444181a8f10498c9519febb
karinazhangchun <chunzhan@andrew.cmu.edu>
2023-04-06 21:16:01 -0400

    assets and other js

---
+++ App.js
@@ -0,0 +1,39 @@
+import React, { useContext } from 'react';
+import { AuthProvider, AuthContext } from './src/AuthContext';
+import Login from './src/screens/Login';
+import SignUp from './src/screens/SignUp';
+import MainApp from './src/MainApp';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const AppContent = () => {
+  const { isAuthenticated } = useContext(AuthContext);
+  const ScreenStack = createStackNavigator();
+
+  const ScreenStackScreen = () => (
+    <NavigationContainer>
+    <ScreenStack.Navigator screenOptions={{headerShown: false}}>
+    <ScreenStack.Screen name="Login" component={Login} />
+    <ScreenStack.Screen name="SignUp" component={SignUp} />
+    </ScreenStack.Navigator>
+    </NavigationContainer>
+  );
+
+  if (!isAuthenticated) {
+    return (
+        <ScreenStackScreen />
+    );
+  }
+
+  return <MainApp />;
+};
+
+const App = () => {
+  return (
+    <AuthProvider>
+      <AppContent />
+    </AuthProvider>
+  );
+};
+
+export default App;
