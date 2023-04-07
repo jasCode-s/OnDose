9be0ddef78d1511d1c0d72feba1a5aeb1becc2b6
karinazhangchun <chunzhan@andrew.cmu.edu>
2023-04-06 22:25:05 -0400

    Logout Implemented in Settings

--- src/screens/Settings.js
+++ src/screens/Settings.js
@@ -1,11 +1,22 @@
-import React, {} from 'react';
-import { View, StyleSheet } from 'react-native';
+import React, {useContext} from 'react';
+import { View, StyleSheet, TouchableOpacity } from 'react-native';
+import { Text } from 'react-native-elements';
+import { AuthContext } from '../AuthContext';
 import theme from '../theme';
 
 const Settings = ({ navigation }) => {
+  const { setIsAuthenticated } = useContext(AuthContext);
+
+  const handleLogout = () => {
+    setIsAuthenticated(false);
+  };
   return (
     <View style={styles.container}>
-    
+
+      <TouchableOpacity style={styles.button} onPress={handleLogout}>
+            <Text style={styles.TextLogout}>LOG OUT</Text>
+      </TouchableOpacity>
+  
     </View>
   );
 };
@@ -19,6 +30,26 @@ const styles = StyleSheet.create({
     backgroundColor: theme.colors.background,
     rowGap:10
   },
+
+  button: {
+    fontWeight: 'bold',
+    fontSize: 45,
+    color: theme.colors.white,
+    backgroundColor: theme.colors.black,
+    height:60,
+    alignItems: 'center',
+    width:'100%',
+    justifyContent: 'center',
+    marginBottom: 10,
+    marginTop: 10,
+    borderRadius: 18
+  },
+
+  TextLogout: {
+    fontSize: 22,
+    color: 'white',
+    fontWeight: 'bold',
+  }
   
 });
 
