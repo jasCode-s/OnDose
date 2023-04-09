6db8aba88b49125baf46e79e9dcc440fbba927d1
karinazhangchun <chunzhan@andrew.cmu.edu>
2023-04-09 00:59:30 -0400

    Collapsable Details Implemented

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,34 +1,48 @@
-import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
+import React, {useState} from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Image, ScrollView } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
 import theme from '../theme';
+import Collapsible from 'react-native-collapsible';
 
 const CustomListItem = ({ item, onDetailPress, onCompletePress }) => {
   const isCompleted = item.completed;
 
+  // Code adapted from https://snack.expo.dev/embedded/@aboutreact/collapsible-and-accordion-view-in-react-native?iframeId=h1ftiunob9&preview=true&platform=ios&theme=dark on April 8th
+  // Collapse items using Caollapsible
+  const [isCollapsed, setIsCollapsed] = useState(true);
+
   return (
-    <TouchableOpacity style={styles.container} onPress={onDetailPress}>
-      <Image source={{ uri: item.image }} style={styles.image} />
-      <View style={styles.textContainer}>
-        <Text style={styles.title}>{item.title}</Text>
-        <Text style={styles.description}>{item.description}</Text>
-      </View>
-      <TouchableOpacity
-        style={[
-          styles.markerButton,
-          {
-            backgroundColor: isCompleted ? theme.colors.red : theme.colors.green,
-          },
-        ]}
-        onPress={onCompletePress}
-      >
-        <FontAwesome
-          name={isCompleted ? 'times' : 'check'}
-          size={35}
-          color={theme.colors.white}
-        />
+    <View>
+      <TouchableOpacity style={styles.container} onPress={() => setIsCollapsed(!isCollapsed)}>
+        <Image source={{ uri: item.image }} style={styles.image} />
+        <View style={styles.textContainer}>
+          <Text style={styles.title}>{item.title}</Text>
+          <Text style={styles.description}>{item.description}</Text>
+        </View>
+        <TouchableOpacity
+          style={[
+            styles.markerButton,
+            {
+              backgroundColor: isCompleted ? theme.colors.red : theme.colors.green,
+            },
+          ]}
+          onPress={onCompletePress}
+        >
+          <FontAwesome
+            name={isCompleted ? 'times' : 'check'}
+            size={35}
+            color={theme.colors.white}
+          />
+        </TouchableOpacity>
       </TouchableOpacity>
-    </TouchableOpacity>
+      <Collapsible collapsed={isCollapsed} style={styles.collapseView}>
+        <View style={styles.collapseContainer}>
+          <Text style={styles.collapseText1}>Time Left: {item.timesLeft}</Text>
+          <Text style={styles.collapseText2}>Side Effects: </Text>
+          <Text>{item.sideEffects}</Text>
+        </View>
+      </Collapsible>
+    </View>
   );
 };
 
@@ -54,6 +68,7 @@ const styles = StyleSheet.create({
   title: {
     fontSize: theme.text.fontSize.medium,
     fontWeight: theme.text.fontWeight.bold,
+    marginBottom: 7
   },
   description: {
     fontSize: theme.text.fontSize.small,
@@ -67,6 +82,24 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  collapseContainer: {
+    backgroundColor: theme.colors.unselected,
+    padding: 15,
+    width:'100%',
+    borderRadius: theme.imgrRadius,
+  },
+  collapseView:{
+    marginTop: 10,
+    height: 160,
+  },
+  collapseText1: {
+    marginBottom: 10,
+    fontWeight:'bold'
+  },
+  collapseText2: {
+    marginBottom: 4,
+    fontWeight:'bold'
+  }
 });
 
 
--- src/screens/Reminders.js
+++ src/screens/Reminders.js
@@ -2,7 +2,6 @@ import React, { useState, useContext } from 'react';
 import { View, Text, FlatList, StyleSheet } from 'react-native';
 import { ButtonGroup } from 'react-native-elements';
 import { useNavigation } from '@react-navigation/native';
-import { useRoute } from '@react-navigation/native';
 import { Provider, Snackbar } from 'react-native-paper';
 
 import CustomListItem from '../components/ListItem';
@@ -11,22 +10,31 @@ import * as Progress from 'react-native-progress';
 
 const Reminders = () => {
   const fakeData = [
-    { id: 1, title: 'Medication A', description: 'Take 2 Pills', completed: false, time: '8:00 AM', image: 'https://images.albertsons-media.com/is/image/ABS/960104140-ECOM?$ng-ecom-pdp-tn$&defaultImage=Not_Available' },
-    { id: 2, title: 'Medication B', description: 'Take 1 Pill', completed: false, time: '6:00 PM', image: 'https://images.albertsons-media.com/is/image/ABS/960104140-ECOM?$ng-ecom-pdp-tn$&defaultImage=Not_Available' },
-    { id: 3, title: 'Medication C', description: 'Take 1 Pill', completed: false, time: '6:00 PM', image: 'https://images.albertsons-media.com/is/image/ABS/960104140-ECOM?$ng-ecom-pdp-tn$&defaultImage=Not_Available' },
-    { id: 4, title: 'Medication B', description: 'Take 1 Pill', completed: false, time: '8:30 PM', image: 'https://images.albertsons-media.com/is/image/ABS/960104140-ECOM?$ng-ecom-pdp-tn$&defaultImage=Not_Available' },
+    { id: 1, title: 'Medication A', description: 'Take 2 Pills', completed: false, time: '8:00 AM', timesLeft: 1,
+    sideEffects: 'Vivid dreams or nightmares; Short-term feelings of depression; Irritability, Stomach cramps; Diarrhea; Constipation; Decreased appetite', 
+    image: 'https://images.albertsons-media.com/is/image/ABS/960104140-ECOM?$ng-ecom-pdp-tn$&defaultImage=Not_Available' },
+    { id: 2, title: 'Medication B', description: 'Take 1 Pill', completed: false, time: '6:00 PM', 
+    sideEffects: 'burning or stinging; irritation inside the nose; runny nose; sneezing', timesLeft: 2,
+    image: 'https://images.albertsons-media.com/is/image/ABS/960104140-ECOM?$ng-ecom-pdp-tn$&defaultImage=Not_Available' },
+    { id: 3, title: 'Medication C', description: 'Take 1 Pill', completed: false, time: '6:00 PM', timesLeft: 1,
+    sideEffects:'red, peeling or blistering skin; rash; itching; swelling of the face, throat, tongue, lips, eyes, hands, feet, ankles, or lower legs; hoarseness; difficulty breathing or swallowing', 
+    image: 'https://images.albertsons-media.com/is/image/ABS/960104140-ECOM?$ng-ecom-pdp-tn$&defaultImage=Not_Available' },
+    { id: 4, title: 'Medication B', description: 'Take 1 Pill', completed: false, time: '8:30 PM', 
+    sideEffects: 'burning or stinging; irritation inside the nose; runny nose; sneezing', timesLeft: 2,
+    image: 'https://images.albertsons-media.com/is/image/ABS/960104140-ECOM?$ng-ecom-pdp-tn$&defaultImage=Not_Available' },
   ];
 
   const [Meds, setMeds] = useState(fakeData);
   const [selectedIndex, setSelectedIndex] = useState(0);
   const navigation = useNavigation();
 
-  // Code adapted from https://callstack.github.io/react-native-paper/4.0/snackbar.html for bottom snakerbar notification on April 8th, 2023 
+  // Code adapted from https://callstack.github.io/react-native-paper/4.0/snackbar.html for bottom snakerbar notification on April 8th, 2023
+  // Snackbar for reminder to be displayed 
   const [visible, setVisible] = useState(false);
-  const onToggleSnackBar = () => setVisible(!visible);
+  const onShowSnackBar = () => setVisible(true);
   const onDismissSnackBar = () => setVisible(false);
 
-  const handleCompletePress = (MedId, MedTime) => {
+  const handleCompletePress = (MedId, MedTime, MedTitle, MedCompleted) => {
     var currHour = new Date().getHours();
     var currMin = new Date().getMinutes();
     var medTimeLen = MedTime.length;
@@ -50,12 +58,13 @@ const Reminders = () => {
     }
 
     if (!canTake) {
-      onToggleSnackBar();// pop snakerbar notification
+      onShowSnackBar();// pop snakerbar notification
     }
     setMeds((prevMeds) =>
-      prevMeds.map((Med) =>
-        ((Med.id === MedId) && canTake) ? { ...Med, completed: !Med.completed } : Med
-      )
+      prevMeds.map((Med) => 
+        ((Med.id === MedId) && canTake) ? { ...Med, completed: !Med.completed } : Med).map((Med) =>
+          ((Med.title === MedTitle) && canTake) ? { ...Med, timesLeft: MedCompleted ? Med.timesLeft + 1 : Med.timesLeft - 1 } : Med
+        )
     );
   };
 
@@ -118,6 +127,7 @@ const Reminders = () => {
             style={styles.progressBar}
           />
         </View>
+        
         <FlatList
           data={groupedMeds}
           renderItem={({ item }) => (
@@ -127,7 +137,7 @@ const Reminders = () => {
                 <CustomListItem
                   key={Med.id}
                   item={Med}
-                  onCompletePress={() => handleCompletePress(Med.id, Med.time)}
+                  onCompletePress={() => handleCompletePress(Med.id, Med.time, Med.title, Med.completed)}
                 />
               ))}
             </View>
